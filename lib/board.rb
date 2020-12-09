9915889c83bdd2454718bfd45a3b7eae800a89b2
steven huynh <steven.h@live.com>
2020-12-08 16:38:32 -0800

    Add Board class Creates 8x8 grid Add display method

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+class Board
+  attr_reader :board # Board.board (getter)
+  def initialize
+    @board = Array.new(8) { Array.new 8, ' ' } # makes a 8x8 grid
+  end
+
+  def display # prints out the grid
+    # puts '   1  2  3  4  5  6  7  8' # column number
+    puts '   0  1  2  3  4  5  6  7' # column number for debugging
+    board.each_with_index do |arr, idx|
+      # print "#{idx + 1} " # row number
+      print "#{idx} " # row number for debugging
+      arr.each do |ele|
+        print "[#{ele}]"
+      end
+      puts
+    end
+  end
+end
